b8521b9daa8768b7ea69b7aa7f4d2d5f51fbed19
Maxim Sadovshikov <110821145+Don4anin@users.noreply.github.com>
2022-10-06 20:16:14 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,26 +9,17 @@ $email = $_POST['email'];
 $tel = $_POST['tel'];
 $message = $_POST['message'];
 
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'm.sadovshikov@yandex.ru';
+$mail->Password = '******';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  					  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'm.sadovshikov@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Q4p2vDaY'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('m.sadovshikov@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('don4anin93@gmail.com');     // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('m.sadovshikov@yandex.ru');
+$mail->addAddress('don4anin93@gmail.com');
+$mail->isHTML(true);
 
 $mail->Subject = 'Новое письмо';
 $mail->Body    = '' .$name . ' прислал вам письмо, его телефон ' .$tel. '<br>Почта этого пользователя: ' .$email. '<br>Текст письма: ' .$message;
